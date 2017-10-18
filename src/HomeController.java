c9f1665917aa0bac3e33ccd3aad8cf2cb7eefc2a
saksham16085 <saksham16085@iiitd.ac.in>
2017-10-18 20:47:11 +0530

    Add files via upload

--- src/HomeController.java
+++ src/HomeController.java
@@ -1,7 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,7 +12,7 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 
-public class LoginController implements Initializable {
+public class HomeController implements Initializable {
 	@FXML
 	public Button Login;
 	// Event Listener on Button.onAction
@@ -21,7 +20,7 @@ public class LoginController implements Initializable {
 	public void login(ActionEvent event) throws IOException {
 		Parent homepage = FXMLLoader.load(getClass().getResource("Home.fxml"));
 		System.out.println("asdasd");
-		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Stage stage = new Stage();
         stage.setTitle("My New Stage Title");
         stage.setScene(new Scene(homepage, 300, 275));
         stage.show();
